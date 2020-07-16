6a4707cc053de4ef2a5fc31ea32cf487f3343e67
Ider Delzo <soloidx@gmail.com>
2020-07-16 17:40:12 -0500

    everycheese

---
+++ everycheese/everycheese/cheeses/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from model_utils.models import TimeStampedModel
+from autoslug import AutoSlugField
+
+
+class Cheese(TimeStampedModel):
+    class Firmess(models.TextChoices):
+        UNSPECIFIED = "unspecified", "Unspecified"
+        SOFT = "soft", "Soft"
+        SEMI_SOFT = "semi-soft", "Semi-Soft"
+        SEMI_HARD = "semi-hard", "Semi-Hard"
+        HARD = "hard", "Hard"
+
+    name = models.CharField("Name of Cheese", max_length=255)
+    slug = AutoSlugField(
+        "Cheese Address", unique=True, always_update=False, populate_from="name"
+    )
+    description = models.TextField("Description", blank=True)
+    firmess = models.CharField(
+        "Firmess",
+        max_length=20,
+        choices=Firmess.choices,
+        default=Firmess.UNSPECIFIED,
+    )
+
+    def __str__(self):
+        return self.name
